632e4283a62f3eff1dc6325b7f2e52830c915a87
Gordon Hall <gordon@gordonwritescode.com>
2017-04-23 19:56:22 -0400

    test torrc create

---
+++ test/torrc.unit.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const proxyquire = require('proxyquire');
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+
+describe('@module granax/torrc', function() {
+
+  describe('@exports', function() {
+
+    it('should write the torrc to tmp', function() {
+      const _mkdirpSync = sinon.stub();
+      const _writeFileSync = sinon.stub();
+      const torrc = proxyquire('../lib/torrc', {
+        mkdirp: {
+          sync: _mkdirpSync
+        },
+        fs: {
+          writeFileSync: _writeFileSync
+        }
+      });
+      const result = torrc();
+      expect(_mkdirpSync.called).to.equal(true);
+      expect(_writeFileSync.called).to.equal(true);
+      expect(typeof result).to.equal('string');
+    });
+
+  });
+
+});
